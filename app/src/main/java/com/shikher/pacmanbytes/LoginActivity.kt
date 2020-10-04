6eed57db1adaa049613bbf813bf5368be74e5415
Shikher Srivastava <shikher707@gmail.com>
2020-10-04 08:14:35 +0530

    Implement Fingerprint Login Functionality

--- app/src/main/java/com/shikher/pacmanbytes/LoginActivity.kt
+++ app/src/main/java/com/shikher/pacmanbytes/LoginActivity.kt
@@ -1,26 +1,43 @@
 package com.shikher.pacmanbytes
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.andrognito.pinlockview.IndicatorDots
 import com.andrognito.pinlockview.PinLockListener
 import com.andrognito.pinlockview.PinLockView
+import com.github.pwittchen.rxbiometric.library.RxBiometric
+import com.github.pwittchen.rxbiometric.library.throwable.AuthenticationError
+import com.github.pwittchen.rxbiometric.library.throwable.AuthenticationFail
+import com.github.pwittchen.rxbiometric.library.throwable.BiometricNotSupported
+import com.github.pwittchen.rxbiometric.library.validation.RxPreconditions
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
 
 
 class LoginActivity: AppCompatActivity() {
 
     val TAG = "PinLockView"
 
-    private lateinit var mPinLockView:PinLockView
+    private var disposable: Disposable? = null
+    private lateinit var mPinLockView: PinLockView
     private lateinit var mIndicatorDots: IndicatorDots
 
     private val mPinLockListener: PinLockListener = object : PinLockListener {
         override fun onComplete(pin: String) {
             Log.d(TAG, "Pin complete: $pin")
+            if (pin == "1234") {
+                gotoHomePage("authenticated!")
+            }
         }
 
         override fun onEmpty() {
@@ -46,8 +63,8 @@ class LoginActivity: AppCompatActivity() {
     private fun intiView() {
 
 
-        mPinLockView = findViewById(R.id.pin_lock_view);
-        mIndicatorDots =  findViewById(R.id.indicator_dots);
+        mPinLockView = findViewById(R.id.pin_lock_view)
+        mIndicatorDots = findViewById(R.id.indicator_dots)
 
         mPinLockView.attachIndicatorDots(mIndicatorDots);
         mPinLockView.setPinLockListener(mPinLockListener);
@@ -55,8 +72,63 @@ class LoginActivity: AppCompatActivity() {
         //mPinLockView.enableLayoutShuffling();
 
         mPinLockView.setPinLength(4);
-        mPinLockView.setTextColor(ContextCompat.getColor(this, R.color.black));
+        mPinLockView.setTextColor(ContextCompat.getColor(this, R.color.black))
 
-        mIndicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION);
+        mIndicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION)
+
     }
+
+    private fun showMessage(message: String) {
+        Toast
+            .makeText(
+                this,
+                message,
+                Toast.LENGTH_SHORT
+            )
+            .show()
     }
+
+    private fun gotoHomePage(message: String) {
+        val intent = Intent(this, BalanceActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        disposable =
+            RxPreconditions
+                .hasBiometricSupport(this)
+                .flatMapCompletable {
+                    if (!it) Completable.error(BiometricNotSupported())
+                    else
+                        RxBiometric
+                            .title("Login using the Fingerprint")
+                            .description("")
+                            .negativeButtonText("Cancel")
+                            .negativeButtonListener(DialogInterface.OnClickListener { _, _ ->
+                                showMessage("Cancel")
+                            })
+                            .executor(ActivityCompat.getMainExecutor(this))
+                            .build()
+                            .authenticate(this)
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onComplete = {
+                        gotoHomePage("authenticated!")
+                    },
+                    onError = {
+                        when (it) {
+                            is AuthenticationError -> showMessage("error: ${it.errorCode} ${it.errorMessage}")
+                            is AuthenticationFail -> showMessage("fail")
+                            else -> {
+                                showMessage("other error")
+                            }
+                        }
+                    }
+                )
+
+    }
+}
